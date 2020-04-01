a680b74dae7f4ec440804efbbcbf0a3fd42f3bc9
nathanial292 <nathanial55@gmail.com>
2020-04-01 19:44:47 +0100

    1.1.5

---
+++ src/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const episode = require('./services/episode')
+const anime = require('./services/anime')
+const season = require('./services/season')
+
+module.exports = {
+  anime,
+  episode,
+  season
+}
